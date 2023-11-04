de9455121f1e81bc5df522e7089fca1d55e99816
GJH <wngud05@naver.com>
2023-11-05 02:23:01 +0900

    게시글 관련 모든 데이터 dto에 담음, 게시글 목록 뷰 수정

--- src/main/java/study/project/dto/BoardDto.java
+++ src/main/java/study/project/dto/BoardDto.java
@@ -3,10 +3,22 @@ package study.project.dto;
 import java.util.Date;
 
 public class BoardDto {
+    private int id;
     private String title;
     private String content;
     private String author;
     private Date created_at;
+    private Date updated_at;
+    private int view_count;
+    private boolean is_deleted;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 
     public String getTitle() {
         return title;
@@ -38,5 +50,29 @@ public class BoardDto {
 
     public void setCreated_at(Date created_at) {
         this.created_at = created_at;
+    }
+
+    public Date getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(Date updated_at) {
+        this.updated_at = updated_at;
+    }
+
+    public int getView_count() {
+        return view_count;
+    }
+
+    public void setView_count(int view_count) {
+        this.view_count = view_count;
+    }
+
+    public boolean isIs_deleted() {
+        return is_deleted;
+    }
+
+    public void setIs_deleted(boolean is_deleted) {
+        this.is_deleted = is_deleted;
     }
 }
