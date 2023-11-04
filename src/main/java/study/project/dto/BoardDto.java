bc0febeb974bb76ec299f8150dd0934d32ec4874
GJH <wngud05@naver.com>
2023-11-05 01:59:26 +0900

    게시글 작성시 '작성 시간' 추가

--- src/main/java/study/project/dto/BoardDto.java
+++ src/main/java/study/project/dto/BoardDto.java
@@ -1,10 +1,12 @@
 package study.project.dto;
 
+import java.util.Date;
+
 public class BoardDto {
     private String title;
     private String content;
-
     private String author;
+    private Date created_at;
 
     public String getTitle() {
         return title;
@@ -28,5 +30,13 @@ public class BoardDto {
 
     public void setAuthor(String author) {
         this.author = author;
+    }
+
+    public Date getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Date created_at) {
+        this.created_at = created_at;
     }
 }
--- src/main/java/study/project/service/BoardServiceImpl.java
+++ src/main/java/study/project/service/BoardServiceImpl.java
@@ -1,22 +1,26 @@
 package study.project.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import study.project.dto.BoardDto;
 import study.project.repository.BoardRepository;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
 public class BoardServiceImpl implements BoardService {
 
     private final BoardRepository boardRepository;
-
+    @Autowired
     public BoardServiceImpl(BoardRepository boardRepository) {
         this.boardRepository = boardRepository;
     }
 
     @Override
     public void save(BoardDto boardDto) {
+        boardDto.setCreated_at(new Date());
+        System.out.println(boardDto.getCreated_at());
         boardRepository.saveBoard(boardDto);
     }
 
